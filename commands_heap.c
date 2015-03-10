df963ce80be6fea80a67cd7ce938e2ad361c1f8f
Daniel Andrus <danieleandrus@gmail.com>
2015-03-10 07:09:34 -0600

    Added functionality to heap commands

--- commands_heap.c
+++ commands_heap.c
@@ -15,5 +15,100 @@ extern struct stmt* cache;
 extern int cache_size;
 extern int cp;
 
-int cmd_heap_store() { return 0; } // TODO
-int cmd_heap_retrieve() { return 0; } // TODO
+int cmd_heap_store()
+{
+    int i, j, addr;
+    
+    // Make sure we have stuff to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Get the heap address
+    addr = stack[sp-2];
+    
+    // Verify address is valid
+    if (addr < 0)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    j = addr % HEAP_MAX;
+    
+    // Attempt to store top item in stack into heap
+    // 1. Must be at least 1 open slot
+    // 2. Must not already exist
+    for (i = 0; i <= HEAP_MAX; i++, j++)
+    {
+        if (j > HEAP_MAX) j = j % HEAP_MAX;
+        
+        // If we made full loop, no open spots
+        if (i == HEAP_MAX)
+        {
+            // TODO HEAP FULL
+            return -1;
+        }
+        
+        // Check for clear spot or a matching address. If found, take it!
+        if (heap[j] == -1 || heap[j] == addr)
+        {
+            heap[j] = addr;
+            heapvals[j] = stack[sp-1];
+            return 0;
+        }
+    }
+    
+    // We shouldn't ever get here. If we do, something's wrong.
+    return -1;
+}
+
+int cmd_heap_retrieve()
+{
+    int i, j, addr;
+    
+    // Make sure we have stuff to work with
+    if (sp < 1)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Get the heap address
+    addr = stack[sp-1];
+    
+    // Verify address is valid
+    if (addr < 0)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    j = addr % HEAP_MAX;
+    
+    // Attempt to store top item in stack into heap
+    // 1. Must be at least 1 open slot
+    // 2. Must not already exist
+    for (i = 0; i <= HEAP_MAX; i++, j++)
+    {
+        if (j > HEAP_MAX) j = j % HEAP_MAX;
+        
+        // If we made full loop, address not allocated
+        if (i == HEAP_MAX)
+        {
+            // TODO ADDRESS NOT ALLOCATED
+            return -1;
+        }
+        
+        // Check for matching address. If found, push to stack!
+        if (heap[j] == addr)
+        {
+            return cmd_stack_push(heapvals[j]);
+        }
+    }
+    
+    // We shouldn't ever get here. If we do, something's wrong.
+    return -1;
+}
