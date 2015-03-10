6a9494ccba7bb2d460409d8b726f1f546809fbd4
Daniel Andrus <danieleandrus@gmail.com>
2015-03-10 06:58:23 -0600

    Added functionality for arithmetic (math) commands

--- commands_math.c
+++ commands_math.c
@@ -15,8 +15,77 @@ extern struct stmt* cache;
 extern int cache_size;
 extern int cp;
 
-int cmd_math_add() { return 0; } // TODO
-int cmd_math_subtract() { return 0; } // TODO
-int cmd_math_multiply() { return 0; } // TODO
-int cmd_math_divide() { return 0; } // TODO
-int cmd_math_modulo() { return 0; } // TODO
+int cmd_math_add()
+{
+    // Make sure we have something to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Add top two items on stack together, pop first, replace second with result
+    stack[sp-2] = stack[sp-1] + stack[sp-2];
+    sp = sp - 1;
+    return 0;
+}
+
+int cmd_math_subtract()
+{
+    // Make sure we have something to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Subtract top two items on stack from each other, pop first, replace second with result
+    stack[sp-2] = stack[sp-1] - stack[sp-2];
+    sp = sp - 1;
+    return 0;
+}
+
+int cmd_math_multiply()
+{
+    // Make sure we have something to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Multiply top two items on stack together, pop first, replace second with result
+    stack[sp-2] = stack[sp-1] * stack[sp-2];
+    sp = sp - 1;
+    return 0;
+}
+
+int cmd_math_divide()
+{
+    // Make sure we have something to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Divide top two items on stack together, pop first, replace second with result
+    stack[sp-2] = stack[sp-1] / stack[sp-2];
+    sp = sp - 1;
+    return 0;
+}
+
+int cmd_math_modulo()
+{
+    // Make sure we have something to work with
+    if (sp < 2)
+    {
+        // TODO ILLEGAL ACCESS
+        return -1;
+    }
+    
+    // Modulo top two items on stack together, pop first, replace second with result
+    stack[sp-2] = stack[sp-1] % stack[sp-2];
+    sp = sp - 1;
+    return 0;
+}
