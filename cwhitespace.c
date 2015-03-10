0a96da5c69181e72b7fc63b40ac99773ec87c8a1
Daniel Andrus <danieleandrus@gmail.com>
2015-03-10 04:18:57 -0600

    Added lovely parse tree

--- cwhitespace.c
+++ cwhitespace.c
@@ -15,48 +15,141 @@
 
 
 // Global variables (stack, heap, etc)
-int pc;							// Program counter
+int pc;                                 // Program counter
 
-num stack[STACK_MAX];			// Stack array
-int sp;							// Index of next open spot in stack
+num stack[STACK_MAX];                   // Stack array
+int sp;                                 // Index of next open spot in stack
 
-num labels[LABEL_MAX];			// Map of labels (index corresponds to index in labelptrs
-int labelbals[LABEL_MAX];		// Map of program counter points
+num labels[LABEL_MAX];                  // Map of labels (index corresponds to index in labelptrs
+int labelbals[LABEL_MAX];               // Map of program counter points
 
-num heap[HEAP_MAX];				// Map for heap addresses
-num heapval[HEAP_MAX];			// Values in heap
+num heap[HEAP_MAX];                     // Map for heap addresses
+num heapval[HEAP_MAX];                  // Values in heap
 
-struct stmt* cache;				// Dynamic array for cached values
-int cp;							// Pointer into next open spot in cache
+struct stmt* cache;                     // Dynamic array for cached values
+int cp;                                 // Pointer into next open spot in cache
 
-
+// Fancy parse lookup tree for fast parsing
+cmd_type ptree[120] = {
+    // space            // tab              // lf
+    INCOMPLETE,         INCOMPLETE,         INCOMPLETE,     //
+    STACK_PUSH,         INCOMPLETE,         INCOMPLETE,     // space
+    INCOMPLETE,         INCOMPLETE,         INCOMPLETE,     // tab
+    INCOMPLETE,         INCOMPLETE,         INCOMPLETE,     // lf
+    INVALID,            INVALID,            INVALID,        // space space
+    STACK_COPY,         INVALID,            STACK_SLIDE,    // space tab
+    STACK_DUPLICATE,    STACK_SWAP,         STACK_POP,      // space lf
+    INCOMPLETE,         INCOMPLETE,         INVALID,        // tab space
+    HEAP_STORE,         HEAP_RETRIEVE,      INVALID,        // tab tab
+    INCOMPLETE,         INCOMPLETE,         INVALID,        // tab lf
+    FLOW_MARK,          FLOW_SUBROUTINE,    FLOW_GOTO,      // lf space
+    FLOW_GOTO_ZERO,     FLOW_GOTO_NEGATIVE, FLOW_RETURN,    // lf tab
+    INVALID,            INVALID,            FLOW_EXIT,      // lf lf
+    INVALID,            INVALID,            INVALID,        // space space space
+    INVALID,            INVALID,            INVALID,        // space space tab
+    INVALID,            INVALID,            INVALID,        // space space lf
+    INVALID,            INVALID,            INVALID,        // space tab space
+    INVALID,            INVALID,            INVALID,        // space tab tab
+    INVALID,            INVALID,            INVALID,        // space tab lf
+    INVALID,            INVALID,            INVALID,        // space lf space
+    INVALID,            INVALID,            INVALID,        // space lf tab
+    INVALID,            INVALID,            INVALID,        // space lf lf
+    MATH_ADD,           MATH_SUBTRACT,      MATH_MULTIPLY,  // tab space space
+    MATH_DIVIDE,        MATH_MODULO,        INVALID,        // tab space tab
+    INVALID,            INVALID,            INVALID,        // tab space lf
+    INVALID,            INVALID,            INVALID,        // tab tab space
+    INVALID,            INVALID,            INVALID,        // tab tab tab
+    INVALID,            INVALID,            INVALID,        // tab tab lf
+    IO_PRINT_CHAR,      IO_PRINT_NUM,       INVALID,        // tab lf space
+    IO_GET_CHAR,        IO_GET_NUM,         INVALID,        // tab lf tab
+    INVALID,            INVALID,            INVALID,        // tab lf lf
+    INVALID,            INVALID,            INVALID,        // lf space space
+    INVALID,            INVALID,            INVALID,        // lf space tab
+    INVALID,            INVALID,            INVALID,        // lf space lf
+    INVALID,            INVALID,            INVALID,        // lf tab space
+    INVALID,            INVALID,            INVALID,        // lf tab tab
+    INVALID,            INVALID,            INVALID,        // lf tab lf
+    INVALID,            INVALID,            INVALID,        // lf lf space
+    INVALID,            INVALID,            INVALID,        // lf lf tab
+    INVALID,            INVALID,            INVALID         // lf lf lf
+};
 
 // Function prototypes
-
+char ws_fgetc(FILE* in);
 
 
 int main(int argc, char** argv)
 {
-	FILE *in;
-	
-	// Print help if incorrect number of arguments were provided
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage:\n  %s <whitespace file>\n", argv[0]);
-		exit(1);
-	}
-	
-	// Open input file
-	in = fopen(argv[1], "r");
-	
-	// Check for errors
-	if (in == NULL)
-	{
-		fprintf(stderr, "%s: Unable to open file for input\n", argv[1]);
-		exit(1);
-	}
-	
-	fclose(in);
-	
-	return 0;
+    FILE *in;                           // Pointer to input file
+    char c;                             // Temporary variable for input
+    int treeptr = 0;                    // Current index into parse tree
+    
+    // Print help if incorrect number of arguments were provided
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage:\n  %s <whitespace file>\n", argv[0]);
+        exit(1);
+    }
+    
+    // Open input file
+    in = fopen(argv[1], "r");
+    
+    // Check for errors
+    if (in == NULL)
+    {
+        fprintf(stderr, "%s: Unable to open file for input\n", argv[1]);
+        exit(1);
+    }
+    
+    // Read until end of file or an error occures
+    while (1)
+    {
+        c = ws_fgetc(in);
+        
+        // Check if error/end of file, break if so
+        if (c == EOF) break;
+        
+        
+        
+    }
+    
+    // Check if we broke because of an error
+    if (ferror(in))
+    {
+        fprintf(stderr, "Error occured while reading from file\n");
+        exit(1);
+    }
+
+    fclose(in);
+    
+    return 0;
+}
+
+
+// Read in characters until error, end of file, or valid whitespace char hit
+char ws_fgetc(FILE* in)
+{
+    char c;
+    
+    // Loop forever
+    do
+    {
+        c = fgetc(in);
+        
+        switch(c)
+        {
+        case ' ':
+        case '\t':
+        case '\n':
+        case EOF:
+            return c;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    while (1);
+    
+    return c;
 }
--- defines.h
+++ defines.h
@@ -7,15 +7,50 @@
 
 
 // Global typedefs and structs
-typedef int num;				// Datatype for numbers and labels alike
+typedef int num;                        // Datatype for numbers and labels alike
 
-typedef enum {					// Numeric constants of commands
-	TEST
+typedef enum {                          // Numeric constants of commands
+    INVALID,                            // Indicates invalid command (char sequence not defined by language)
+    INCOMPLETE,                         // Indicates incomplete command (similar, used for parsing)
+    
+    STACK_PUSH,                         // Push number onto stack
+    STACK_DUPLICATE,                    // Copy number on top of stack onto top of stack (again)
+    STACK_COPY,                         // Copy nth item on stack (given by argument) onto top of stack
+    STACK_SWAP,                         // Swap top two items on stack
+    STACK_POP,                          // Discard top item on stack
+    STACK_SLIDE,                        // Slide n items (given by argument) off stack, keeping top one
+    
+    MATH_ADD,                           // Add top item instack by second item in stack, replace them with result
+    MATH_SUBTRACT,                      // Subtract top item in stack by second item in stack, replace them with result
+    MATH_MULTIPLY,                      // Multiply top item in stack by second item in stack, replace them with result
+    MATH_DIVIDE,                        // Integer divide top item in stack by second item in stack, replace them with result
+    MATH_MODULO,                        // Mod top item in stack by second item in stack, replace them with result
+    
+    HEAP_STORE,                         // Store value at top of stack at location found immediately under it in stack
+    HEAP_RETRIEVE,                      // Retrieve value in heap at location found at top of stack
+    
+    FLOW_MARK,                          // Mark location in program
+    FLOW_SUBROUTINE,                    // Call subroutine
+    FLOW_GOTO,                          // GOTO label unconditionally
+    FLOW_GOTO_ZERO,                     // GOTO label if top of stack is zero
+    FLOW_GOTO_NEGATIVE,                 // GOTO label if top of stack is negative
+    FLOW_RETURN,                        // End subroutine, transfer control back to caller
+    FLOW_EXIT,                          // Exit the program
+    
+    IO_PRINT_CHAR,                      // Output character at top of stack
+    IO_PRINT_NUM,                       // Output number at top of stack
+    IO_GET_CHAR,                        // Read character and place it at location given by top of stack
+    IO_GET_NUM                          // Read number and place it at location given by top of stack
 }
 cmd_type;
 
-typedef struct stmt {
-	cmd_type cmd;
-	num arg;
-} stmt;
+typedef enum {
+    SPACE = 0,
+    TAB = 1,
+    LINEFEED = 2
+} char_type;
 
+typedef struct stmt {
+    cmd_type cmd;
+    num arg;
+} stmt;
